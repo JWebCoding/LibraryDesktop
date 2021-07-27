1f9ffff9d7d0d57bb5f8f44f5e2e0fe287b55692
Joshua Weber <63745585+JOHONDAR@users.noreply.github.com>
2021-07-26 20:55:27 -0500

    Various changes
    Added method to open the details screen
    Wrapped some methods in try/catch statements
    Changed the "Language" radio button to "Series Name"
    Several smaller changes to adjust functionality for the new detail screen and search.

--- src/Controllers/SearchBookController.java
+++ src/Controllers/SearchBookController.java
@@ -4,10 +4,16 @@ import Models.Book;
 import Models.ConnectionCommands;
 import Models.SQLCommands;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 
 import javax.sql.rowset.CachedRowSet;
+
+import java.io.IOException;
 import java.sql.SQLException;
 
 public class SearchBookController {
@@ -21,29 +27,35 @@ public class SearchBookController {
     @FXML TableColumn<Book,String> tableColumnISBN;
     @FXML TableColumn<Book,Integer> tableColumnYear;
     @FXML TableColumn<Book,String> tableColumnGenre;
-    @FXML TableColumn<Book,String> tableColumnFormat;
+    @FXML TableColumn<Book,Integer> tableColumnSeriesPart;
     @FXML TableColumn<Book,String> tableColumnSeries;
+    @FXML TableColumn<Book,Integer> tableColumnId;
 
     @FXML TextField textFieldSearch;
     @FXML Label labelNotification;
 
     @FXML ToggleGroup toggleSearchButtons;
+    
+    @FXML Button buttonEditBook;
 
     // Declare variables
     String title,firstName,lastName,isbn,series,publisher,genre,language,searchText,notification;
-    int id,series_part,year,format,edition,finished;
+    int id,year,format,edition,finished;
+    Integer seriesPart;
     CachedRowSet bookCollection=null;
 
 
     public void initialize() throws Exception{
+    	tableColumnId.setCellValueFactory(new PropertyValueFactory("id"));
         tableColumnTitle.setCellValueFactory(new PropertyValueFactory("title"));
         tableColumnSeries.setCellValueFactory(new PropertyValueFactory("series"));
+        tableColumnSeriesPart.setCellValueFactory(new PropertyValueFactory("seriesPart"));
         tableColumnAuthor.setCellValueFactory(new PropertyValueFactory("author"));
         tableColumnPublisher.setCellValueFactory(new PropertyValueFactory("publisher"));
         tableColumnISBN.setCellValueFactory(new PropertyValueFactory("isbn"));
         tableColumnYear.setCellValueFactory(new PropertyValueFactory("copyright"));
         tableColumnGenre.setCellValueFactory(new PropertyValueFactory("genre"));
-        tableColumnFormat.setCellValueFactory(new PropertyValueFactory("format"));
+        
 
         getBookCollectionData();
         fillTable();
@@ -60,19 +72,18 @@ public class SearchBookController {
         id=bookCollection.getInt("bookID");
         title=bookCollection.getString("title");
         series=bookCollection.getString("series_name");
-        series_part=bookCollection.getInt("series_part");
+        seriesPart=bookCollection.getInt("series_part");
+        if(seriesPart==0) {
+        	seriesPart=null;
+        }
         firstName=bookCollection.getString("firstName");
         lastName=bookCollection.getString("lastName");
         publisher=bookCollection.getString("publisher_name");
         isbn=bookCollection.getString("isbn");
         year=bookCollection.getInt("copyright");
         genre=bookCollection.getString("genre_name");
-        format=bookCollection.getInt("format");
-//            edition=bookCollection.getInt("edition");
-//            language=bookCollection.getString("language");
-//            finished=bookCollection.getInt("finished");
 
-        return new Book(id,title,series,firstName,lastName,publisher,isbn,year,genre,format);
+        return new Book(id,title,series,seriesPart,firstName,lastName,publisher,isbn,year,genre);
     }
 
     // Fill the search table with book information
@@ -118,8 +129,8 @@ public class SearchBookController {
                 case "Genre":
                     selectedColumn="genre_name";
                     break;
-                case "Language":
-                    selectedColumn="language";
+                case "Series Name":
+                    selectedColumn="series_name";
                     break;
             }
 
@@ -154,16 +165,20 @@ public class SearchBookController {
 
     // Reset the table columns to their default width
     public void resetColumnWidths() throws Exception {
-
-        tableColumnFormat.setPrefWidth(61);
-        tableColumnGenre.setPrefWidth(100);
-        tableColumnYear.setPrefWidth(54);
-        tableColumnISBN.setPrefWidth(117);
-        tableColumnPublisher.setPrefWidth(112);
-        tableColumnAuthor.setPrefWidth(94);
-        tableColumnSeries.setPrefWidth(99);
-        tableColumnTitle.setPrefWidth(161);
-    }
+    	try {
+    		tableColumnSeriesPart.setPrefWidth(61);
+            tableColumnGenre.setPrefWidth(100);
+            tableColumnYear.setPrefWidth(54);
+            tableColumnISBN.setPrefWidth(117);
+            tableColumnPublisher.setPrefWidth(112);
+            tableColumnAuthor.setPrefWidth(94);
+            tableColumnSeries.setPrefWidth(99);
+            tableColumnTitle.setPrefWidth(161);
+    	}
+    	catch(Exception e) {
+        	e.printStackTrace();
+        } 
+    } 
 
     // Reset the contents of the table
     public void resetTableContents() throws Exception {
@@ -188,5 +203,27 @@ public class SearchBookController {
 
     public void printTest(int num){
         System.out.println("TEST"+num);
+    }
+    
+    public void openScreenDetails() throws IOException {
+    	if(tableViewBooks.getSelectionModel().getSelectedItem()!=null) {
+	    	Parent root;
+	    	try {
+	    		Book detailsBook=tableViewBooks.getSelectionModel().getSelectedItem();
+	    		Book.setBookID(detailsBook.getId());
+
+	    		root=FXMLLoader.load(getClass().getClassLoader().getResource("FXML/details_edit.fxml"));
+	    		Stage detailStage=new Stage();
+	    		detailStage.setTitle("Book Details");
+	    		detailStage.setScene(new Scene(root,563,450));
+	    		detailStage.show();
+	    	} catch(IOException e){
+	    		e.printStackTrace();
+	    	}
+    	}
+    	else {
+    		showNotification("Please select a row\nto show details");
+    	}
+    	
     }
 }
