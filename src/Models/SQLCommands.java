70a6ab286b2daa847027aa6f86710cec9cf99ac6
Joshua Weber <63745585+JOHONDAR@users.noreply.github.com>
2021-10-04 11:14:48 -0500

    Middle Name update and Book Removal addition
    Updated the queries for selecting books so that the middle names are now included.
    
    Added the query for removing books.

--- src/Models/SQLCommands.java
+++ src/Models/SQLCommands.java
@@ -10,16 +10,18 @@ public class SQLCommands {
     public String selectAllUser="select * from library.user;";
     public String selectAllLanguage="select * from library.language order by language_name;";
     // Primary queries
-    public String selectTableBookInfo="select bookID,title,series_name,series_part,firstName,lastName,publisher_name,isbn,edition,copyright,genre_name,format,pages,language_name from book join language l on book.languageID=l.languageID join publisher p on book.publisherID = p.publisherID join author a on book.authorID = a.authorID join series s on book.seriesID = s.seriesID join genre g on book.genreID = g.genreID;";
+    public String selectTableBookInfo="select bookID,title,series_name,series_part,firstName,middleName,lastName,publisher_name,isbn,edition,copyright,genre_name,format,pages,language_name from book join language l on book.languageID=l.languageID join publisher p on book.publisherID = p.publisherID join author a on book.authorID = a.authorID join series s on book.seriesID = s.seriesID join genre g on book.genreID = g.genreID;";
     
     public String selectNewAuthorID="select authorID from author where firstName='%s' and lastName='%s' and nation='%s';";
-    public String selectSpecificBook="select bookID,title,series_name,series_part,firstName,lastName,publisher_name,isbn,edition,copyright,genre_name,format,pages,language_name from book join language l on book.languageID=l.languageID join publisher p on book.publisherID = p.publisherID join author a on book.authorID = a.authorID join series s on book.seriesID = s.seriesID join genre g on book.genreID = g.genreID where bookID='%s';";
+    public String selectSpecificBook="select bookID,title,series_name,series_part,firstName,middlename,lastName,publisher_name,isbn,edition,copyright,genre_name,format,pages,language_name from book join language l on book.languageID=l.languageID join publisher p on book.publisherID = p.publisherID join author a on book.authorID = a.authorID join series s on book.seriesID = s.seriesID join genre g on book.genreID = g.genreID where bookID='%s';";
     // Insert statements
-    public String insertIntoAuthor="insert into library.author (firstName, lastName, nation, birth, death) values ('%s','%s','%s',%d,%d);";
+    public String insertIntoAuthor="insert into library.author (firstName,middleNmae, lastName, nation, birth, death) values ('%s','%s','%s','%s',%d,%d);";
     public String insertIntoPublisher="insert into library.publisher (publisher_name, publisher_location) values ('%s','%s');";
     public String insertIntoGenre="insert into library.genre (genre_name, genre_type) values ('%s',%d);";
     public String insertIntoSeries="insert into library.series (series_name) values ('%s'); ";
     public String insertIntoLanguage="insert into library.language (language_Name, language_suffix) values ('%s','%s');";
     public String insetIntoBook="insert into library.book (authorID, publisherID, title, copyright, isbn, edition, genreID, series_part, format, pages, languageID, seriesID) VALUES (%d,%d,'%s',%d,%d,%d,%d,%d,%d,%d,%d,%d);";
     public String updateBookToFinished="update library.book set finished = 1 where bookID= '%s';";
+    // Remove statements
+    public String removeBookFromDatabase="Delete from library.book where bookID='%s';";
 }
