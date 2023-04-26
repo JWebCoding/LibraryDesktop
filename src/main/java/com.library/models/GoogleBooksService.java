207ce9d1b42d260654af07c1db4797163fde78bf
Joshua Weber <63745585+JWebCoding@users.noreply.github.com>
2023-04-26 17:15:08 -0500

    Create GoogleBooksService.java

---
+++ src/main/java/com.library/models/GoogleBooksService.java
@@ -0,0 +1,48 @@
+package com.library.models;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class GoogleBooksService {
+
+    public static InputStream callBookAPI(String bookData, String infoType) throws IOException {
+        // Get book information from books api
+        URL url= new URL(constructURL(bookData, infoType));
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+//        InputStream responseStream = connection.getInputStream();
+//        JsonNode volumeInfo = parseResponse(responseStream);
+
+        return connection.getInputStream();
+    }
+
+    private static String constructURL(String bookInfo, String infoType){
+
+        if(infoType.equalsIgnoreCase("isbn")){
+
+        }
+
+        String finalURL;
+        String baseURL="https://www.googleapis.com/books/v1/volumes?q=";
+        String APIKey="&key=AIzaSyDUvX51t5rWFW3zaWsnzK8y_rgJh_OZFOc";
+
+        finalURL=baseURL+infoType+":"+bookInfo+APIKey;
+        return finalURL;
+    }
+
+//    private static JsonNode parseResponse(InputStream responseStream) throws IOException {
+//        // Parse the response and isolate the "volumeInfo" object
+//        ObjectMapper mapper = new ObjectMapper();
+//        JsonFactory factory = mapper.getFactory();
+//        JsonParser parser = factory.createParser(responseStream);
+//        JsonNode node = mapper.readTree(parser);
+//        JsonNode volumeInfo = node.get("items").get(0).get("volumeInfo");
+//
+//        return volumeInfo;
+//    }
+}
